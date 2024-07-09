510120d7f3e504bcd3e5f3a14356d9b6556c1782
Gustavo Eyros <gustavoeyros@hotmail.com>
2024-07-09 16:56:12 -0300

    feat: exceptions and new logic to place a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,8 @@ import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
-  public static void main(String[] args){
-      ChessMatch chessMatch = new ChessMatch();
-      UI.printBoard(chessMatch.getPieces());
-
-  }
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+    }
 }
